6f5003cf71847ac842e092a1fa81e417e537556d
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2022-04-26 22:36:44 +0200

    feat: add bill of materials
    detail an outline for items TODO

--- lib/core.dart
+++ lib/core.dart
@@ -1,3 +1,4 @@
+import 'package:core/src/introduction/materials.dart';
 import 'package:core/src/introduction/distribution.dart';
 import 'package:core/src/introduction/recommended_distribution.dart';
 import 'package:core/src/introduction/welcome.dart';
@@ -10,16 +11,54 @@ final List<Author> authors = [
       twitter: "ZektorH"),
 ];
 
-Chapter introductionChapter = Chapter(
-    heading: Heading(title: "Introduction"),
-    contents: [
-      Welcome(),
-      Distribution(recommendedAppendix: recommendedAppendix)
-    ]);
+Chapter introductionChapter =
+    Chapter(heading: Heading(title: "Introduction"), contents: [
+  Welcome(),
+  Distribution(recommendedAppendix: recommendedAppendix),
+  // TODO: Write a "how to read"
+  // TODO: Introduce the book structure
+  Materials()
+]);
 
 Chapter recommendedAppendix = Chapter(
     heading: Heading(title: "Recommended Expansions and Supplements"),
     contents: [RecommendedDistribution()]);
+
+Chapter basicsChapter =
+    Chapter(heading: Heading(title: "Basic Rules"), contents: [
+  // TODO: Write a "rolling dice", use the `chancesOfSuccess` appendix to link
+  // TODO: Detail test rules
+  // TODO: Detail the attributes
+  // TODO: Explain actions, half-actions and reactions
+  // TODO: Explain spells
+]);
+
+Chapter chancesOfSuccess = Chapter(
+    heading: Heading(title: "Chances of Success and Difficulties"),
+    contents: [
+      // TODO: Create dynamic difficulty table
+    ]);
+
+Chapter charactersChapter =
+    Chapter(heading: Heading(title: "Characters"), contents: [
+  // TODO: Write about what "characters" are
+  // TODO: Share character sheet
+  // TODO: Share rules for filling the character sheet
+  // TODO: Showcase example character creation process
+]);
+
+Chapter combatChapter = Chapter(heading: Heading(title: "Combat"), contents: [
+  // TODO: Write a "intro to combat" detailling what counts as combat
+  // TODO: Explain turn order
+  // TODO: Explain HP, damage
+  // TODO: Explain death, resurrection and unconsciousness
+]);
+
+Chapter explorationChapter =
+    Chapter(heading: Heading(title: "Exploration"), contents: [
+  // TODO: Write a "intro to exploration" sharing how exploration can fit into the game
+  // TODO: Explain how to use combat spells outside of combat
+]);
 
 class CoreRuleBook extends Book {
   CoreRuleBook()
@@ -28,11 +67,15 @@ class CoreRuleBook extends Book {
                 title: "Core Rulebook",
                 subtitle: "The core rules of the powerd6 role-playing system."),
             authors: authors,
-            chapters:
-                ChapterList(heading: Heading(title: "Chapters"), chapters: [
-              introductionChapter,
-            ]),
+            chapters: ChapterList(
+                heading: Heading(title: "Chapters"),
+                chapters: [
+                  introductionChapter,
+                  basicsChapter,
+                  charactersChapter,
+                  combatChapter
+                ]),
             appendices: ChapterList(
                 heading: Heading(title: "Appendices"),
-                chapters: [recommendedAppendix]));
+                chapters: [recommendedAppendix, chancesOfSuccess]));
 }
---
+++ lib/src/introduction/materials.dart
@@ -0,0 +1,31 @@
+import 'package:powerbook/mixins.dart';
+import 'package:powerbook/models.dart';
+
+class Materials extends Content with Indexable {
+  Heading heading = Heading(title: "Materials", headingLevel: 2);
+
+  @override
+  String getId() {
+    return heading.getId();
+  }
+
+  @override
+  String getTitle() {
+    return heading.getTitle();
+  }
+
+  @override
+  String toHtml() {
+    return """
+      ${heading.toHtml()}
+      <p>This book is written considering that the players have a couple of items available to them:</p>
+      <ul>
+        <li>Note-taking material, like pen and paper</li>
+        <li>At least one six-sided die</li>
+        <li>This book</li>
+      </ul>
+      <p>While these items can be easily replaced by digital alternatives, they will be referred to as their physical counterparts for simplicity.</p>
+
+""";
+  }
+}
